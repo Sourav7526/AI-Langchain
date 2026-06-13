892c88119875041aea948a8c66d06303b53f523b
Sourav7526 <sk5164203@gmail.com>
2026-06-13 16:33:02 +0530

    cr

---
+++ CR/Assignment2.py
@@ -0,0 +1,29 @@
+#Strings Assignment:
+
+#Question 1
+# str=input("enter a string")
+# if len(str)<2:
+#     print("string is not valid")
+# else:
+#     print(str[:2] + str[-2:])
+
+
+#Question 2
+# word1=input("enter a word")  #coder
+# word2=input("enter a word")  #roots
+# print(word2[0]  + word1[1:5] + " " + word1[0]  + word2[1:5])
+
+
+#Question 3
+# str=input("enter a string")
+# if str.endswith("ing"):
+#     print(str[:-3] + "ly")
+# else:
+#     str=str + "ing"
+#     print(str)
+ 
+
+#Question 4
+# str=input("enter string")
+# n=int(input("enter index to remove"))
+# print(str[:n] + str[n+1:])
